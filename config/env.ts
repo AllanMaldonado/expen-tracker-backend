f7d1dcb41bbb4879dae7dce2a93ccbb5e33a2742
Allan_Maldonado <allanmsdev@gmail.com>
2025-09-04 19:31:04 -0300

    fix: adding optional envs

--- config/env.ts
+++ config/env.ts
@@ -3,8 +3,9 @@ import { z } from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url().startsWith('postgres://'),
+  DEV_URL: z.string().optional(),
   TELEGRAM_BOT_TOKEN: z.string(),
-  TELEGRAM_WEBHOOK_SECRET: z.string(),
+  TELEGRAM_WEBHOOK_SECRET: z.string().optional(),
 })
 
 export const env = envSchema.parse(process.env)
